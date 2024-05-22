978b754068367eaf26ca67f6daa3157d7e515f32
Liny Thomas <linythomas2@gmail.com>
2024-05-22 12:44:29 +0400

    End of section 4

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification:BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            addInclude(x=>x.ProductType);
+            addInclude(x=>x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id ): base(x=>x.Id==id)
+        {
+            addInclude(x => x.ProductType);
+            addInclude(x => x.ProductBrand);
+        }
+    }
+}
