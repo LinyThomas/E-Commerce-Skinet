6397fc2f8c7864b90205ee5607349f7632d7324e
Liny Thomas <linythomas2@gmail.com>
2024-06-06 20:03:42 +0400

    End of Section 9

--- clientApp/src/app/app.component.ts
+++ clientApp/src/app/app.component.ts
@@ -1,29 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../models/product';
-import { Pagination } from '../models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
-  constructor(private http:HttpClient) {
 
-  }
-  ngOnInit(): void {
-    this.http
-      .get<Pagination<Product[]>>('https://localhost:5001/api/products')
-      .subscribe({
-        next: (response: Pagination<Product[]>) => (this.products = response.data),
-        error: (error) => console.log(error),
-        complete: () => {
-          console.log('Request Completed');
-          console.log('extra statement');
-        },
-      });
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import {  provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    ShopModule
+  ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
